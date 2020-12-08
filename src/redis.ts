e491283b98524ac9390655378773351f3fb8e9c0
Adam Rutter <adamrutter92@gmail.com>
2020-12-08 16:29:50 +0000

    Actually connect to Redis...

--- src/redis.ts
+++ src/redis.ts
@@ -3,6 +3,18 @@ import redis from "redis"
 require("dotenv").config() // eslint-disable-line @typescript-eslint/no-var-requires
 
 export const setupRedis = (): redis.RedisClient => {
-  const client = redis.createClient({ url: process.env.REDIS_URL })
+  const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT) || 6379
+  })
+
+  client.on("error", error => {
+    console.error(error)
+  })
+
+  client.on("connect", () => {
+    console.log("Connected to Redis")
+  })
+
   return client
 }
